f4d71e4b0dd426ae3092b967f9003c71b05d1abb
David Bragg <davidbragg@Davids-iMac.local>
2020-04-16 19:42:59 +1000

    register placeholder

--- src/Components/Pages/Register.js
+++ src/Components/Pages/Register.js
@@ -1,31 +1,11 @@
 import React, { Component } from "react";
-import "./Login.scss";
+import "./Register.scss";
 
 class Register extends Component {
   render() {
     return (
-      <div id="login">
-        <div id="container" className="login-container">
-          <div className="form-container sign-up-container">
-            <h1>Sign Up</h1>
-          </div>
-          <div className="form-container sign-in-container">
-            <h1>Sign In</h1>
-          </div>
-
-          <div className="overlay-container">
-            <div className="overlay">
-              <div className="overlay-panel overlay-left">
-                <h1>Already have an account?</h1>
-                <button className="ghost">Sign In</button>
-              </div>
-              <div className="overlay-panel overlay-right">
-                <h1>Don't have an account?</h1>
-                <button className="ghost">Sign In</button>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div id="register">
+        <h1>Register</h1>
       </div>
     );
   }
