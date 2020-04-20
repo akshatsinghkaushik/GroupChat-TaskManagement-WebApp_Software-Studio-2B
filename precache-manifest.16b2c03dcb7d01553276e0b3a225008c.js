557fb9f068c916a088118b559d94dfa6bc296267
Jahmilli <sebastian.southern@gmail.com>
2020-04-20 08:22:45 +0000

    Deploying to gh-pages from  @ 883a89b121722477b641231f4228c282899f7313 🚀

---
+++ precache-manifest.16b2c03dcb7d01553276e0b3a225008c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a49ffc5be59858f36e1a40cf1671479",
+    "url": "/teams-ui/index.html"
+  },
+  {
+    "revision": "11f3ab274cffbc897d9c",
+    "url": "/teams-ui/static/css/main.22932984.chunk.css"
+  },
+  {
+    "revision": "4f5e138747b95ce2103c",
+    "url": "/teams-ui/static/js/2.32e86f61.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/teams-ui/static/js/2.32e86f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f3ab274cffbc897d9c",
+    "url": "/teams-ui/static/js/main.bb1745c5.chunk.js"
+  },
+  {
+    "revision": "92ab18a5b469776489c3",
+    "url": "/teams-ui/static/js/runtime-main.2d2fc203.js"
+  },
+  {
+    "revision": "b5d56f8dcdc295278d1c9ae33ec0afaf",
+    "url": "/teams-ui/static/media/icon.b5d56f8d.png"
+  },
+  {
+    "revision": "54c7b65325d839726d12639ad8cd9847",
+    "url": "/teams-ui/static/media/wavey.54c7b653.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/teams-ui/precache-manifest.ea3452c437e8fc408a1b6f53174a0788.js"
+  "/teams-ui/precache-manifest.16b2c03dcb7d01553276e0b3a225008c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teams-ui/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
