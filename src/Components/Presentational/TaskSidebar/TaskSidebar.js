15533df2f8700a8c7c4da9219658427619624700
Jahmilli <sebastian.southern@gmail.com>
2020-05-30 12:13:32 +1000

    setup date picker

--- src/Components/Presentational/TaskSidebar/TaskSidebar.js
+++ src/Components/Presentational/TaskSidebar/TaskSidebar.js
@@ -1,11 +1,39 @@
-import React from "react";
+import "date-fns";
+
+import React, { useState } from "react";
 import "./TaskSidebar.scss";
 import TaskDelete from "../TaskDelete/TaskDelete";
+import DateFnsUtils from "@date-io/date-fns";
+
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
 
 const TaskSidebar = ({ columnId, taskDetails, close }) => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const handleDateChange = (date) => {
+    console.log("date is ", date);
+    setSelectedDate(date);
+  };
   return (
     <div className="sidebar">
       <TaskDelete columnId={columnId} taskDetails={taskDetails} close={close} />
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="dd/MM/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Date picker inline"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            "aria-label": "change date",
+          }}
+        />
+      </MuiPickersUtilsProvider>
     </div>
   );
 };
